448a57a3bc797f183f5c7ede74a23bf56f5a32cb
xju <xiangyang.ju@gmail.com>
2018-05-15 18:37:16 -0700

    json files

--- pythia/src/util/testJson.cxx
+++ pythia/src/util/testJson.cxx
@@ -11,14 +11,32 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	Json::Reader reader;
 	Json::Value value;
-	ifstream cfgfile(argv[1]);
+	ifstream cfgfile(argv[1], ifstream::binary);
 	cfgfile >> value;
 
 	cout << "---- start ----" << endl;
 	cout << value << endl;
 	cout << "---- end ----" << endl;
+
+	cout << value.size() << endl;
+	const Json::Value jets = value["Jets"];
+	cout <<"Jets: " << jets.size() << ", type: " << jets.type() << endl;
+	const Json::Value resolution = jets["Run1_Resolution"];
+	cout <<"Jets, Resolution: " << resolution.size() << ", type: " << resolution.type() << endl;
+	const Json::Value pT = resolution["pT"];
+	cout <<"Jets, pT: " << pT.size() << ", type: " << pT.type() << endl;
+	const Json::Value eta = resolution["eta"];
+	if (eta.type() == Json::nullValue){
+		cout << "no eta range, then use [-5, 5]" << endl;
+	}
+
+	for(int index = 0; index < pT.size(); ++index){
+		double jet_pt = pT[index].asDouble();
+		// string jet_pt = pT[index].asString();
+		cout << jet_pt <<" " << endl;
+	}
+
 
 	return 0;
 }
